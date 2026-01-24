bc15adaca8f6be2a9ac4c990bd70f14a3d81f407
Uvindu Anusara <uvindu03@gmail.com>
2026-01-24 21:06:28 +0530

    managing authentication

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() body: any) {
+    return this.authService.register(body);
+  }
+
+  @Post('login')
+  login(@Body() body: any) {
+    return this.authService.login(body.email, body.password);
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import {
+  Injectable,
+  BadRequestException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(data: any) {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    try {
+      const user = await this.prisma.user.create({
+        data: { ...data, password: hashedPassword },
+      });
+      return { message: 'User created successfully', userId: user.id };
+    } catch (e) {
+      throw new BadRequestException('Email already exists');
+    }
+  }
+
+  async login(email: string, pass: string) {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (!user || !(await bcrypt.compare(pass, user.password))) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload = { sub: user.id, email: user.email };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
