152476d303ad34f4dbdbb98db14e7bc8d42ff594
Uvindu Anusara <uvindu03@gmail.com>
2026-01-24 21:23:21 +0530

    authentication with jwt  implementation finished

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      global: true,
+      secret: 'SUPER_SECRET_KEY', // Use process.env.JWT_SECRET in production
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
