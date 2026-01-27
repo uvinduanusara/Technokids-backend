08516d4b45dd907cff2489242cca871bbe1b271c
Uvindu Anusara <uvindu03@gmail.com>
2026-01-27 09:48:03 +0530

    change the backend port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', // Replace with your Next.js URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Required if you pass cookies or Authorization headers
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
